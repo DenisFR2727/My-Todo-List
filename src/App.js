effed4d7207d309dc9d3ae68440540c2f16c0f32
Denis <dh92fr@gmail.com>
2023-08-21 13:48:49 +0300

    todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/Todo/todoList";
+import AddTask from "./components/addTask/addTask";
+import CompletedList from "./components/completed/completedList";
+import { StateValuesProvider } from "./components/context/stateValuesProvider";
+import styled from "styled-components";
+import GlobalStyle from "./components/style/globalStyle";
+
+
+const AppWrapper = styled.div`
+ 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  
+      .content_app{
+        text-align: center;
+        width: 1000px;
+
+      li {
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        padding-top: 10px;
+       
+      div{
+        max-width: 800px;
+        overflow: hidden; /* Приховати вміст, який виходить за межі */
+        text-overflow: ellipsis; /* Додати маркер "..." для неповного вмісту */
+        white-space: nowrap;
+         }
+      
+      } 
+      }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StateValuesProvider>
+      <GlobalStyle />
+      <AppWrapper>
+      <div className="App">
+            <div className='content_app'>
+                <h1>ToDoList</h1>
+                <AddTask />
+                <TodoList />
+                <CompletedList />
+            </div>
+      </div>
+      </AppWrapper>
+    </StateValuesProvider>
   );
 }
 
